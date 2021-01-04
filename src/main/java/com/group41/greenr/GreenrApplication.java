82c5c98839720d8da142e1b917b79098f57a0f90
Selina <mariiaselina@gmail.com>
2021-01-04 14:42:24 +0200

    spring project initialised

---
+++ src/main/java/com/group41/greenr/GreenrApplication.java
@@ -0,0 +1,12 @@
+package com.group41.greenr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenrApplication.class, args);
+	}
+}
